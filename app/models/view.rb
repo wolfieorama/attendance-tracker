0f3ea4339bddff6727c12a4f21f553c3ea2e6b4e
wolfieorama <wachirawamunyi@gmail.com>
2016-01-10 21:32:50 +0300

    added ujs for sign-in animation

--- app/models/view.rb
+++ app/models/view.rb
@@ -1,19 +1,6 @@
-class User < ActiveRecord::Base
-
+class View < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :attendances
-  belongs_to :team
-
-  self.inheritance_column = :type
-
-  def self.types
-    %w(Agent TeamLeader)
-  end
-
-  def admin?
-    :type == "TeamLeader"
-  end
 end
